3ebd0c814d81c937d4d24c97f15b07e73a2cd722
MaksimFelchuck <felchuck@yandex.ru>
2018-11-11 01:06:04 +0700

    обработка формы

--- Script/forms.py
+++ Script/forms.py
@@ -1,14 +1,8 @@
 from django import forms
-from django.contrib.auth.models import User
-
-
-
 from .models import *
 
 
 class ScriptForm(forms.ModelForm):
     class Meta:
         model = script
-        exclude = ['creator']
-
-
+        exclude = ['creator', 'create_date']
