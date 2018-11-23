dd25a3d4cbe71418aafd95787bc0ddd6afecbaa3
MaksimFelchuck <felchuck@yandex.ru>
2018-11-23 11:36:41 +0700

    refresh

--- Script/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(script)
-admin.site.register(History)
-admin.site.register(Script_from_github)
-
